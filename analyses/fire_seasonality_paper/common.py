aa3dc645ef0cfa883b14a9f4d01556686ca193b0
Alexander Kuhn-Regnier <ahk114@ic.ac.uk>
2020-06-11 23:19:18 +0100

    Further work, e.g. on feature importances. Integrated more functions into the common module.

--- analyses/fire_seasonality_paper/common.py
+++ analyses/fire_seasonality_paper/common.py
@@ -10,8 +10,12 @@ from collections import defaultdict
 from functools import partial, reduce
 from itertools import combinations
 from operator import mul
+from pathlib import Path
+from time import time
 
+import cloudpickle
 import dask.distributed
+import eli5
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,13 +24,14 @@ import scipy
 import seaborn as sns
 import shap
 from dask.distributed import Client
-from joblib import parallel_backend
+from hsluv import hsluv_to_rgb, rgb_to_hsluv
+from joblib import Parallel, delayed, parallel_backend
 from loguru import logger as loguru_logger
 from matplotlib.patches import Rectangle
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
-from alepython import ale_plot
+from alepython.ale import _sci_format, ale_plot, first_order_ale_quant
 from pdpbox import pdp
 from wildfires.analysis import (
     FigureSaver,
@@ -44,6 +49,7 @@ from wildfires.dask_cx1 import (
     dask_fit_loco,
     fit_dask_sub_est_grid_search_cv,
     get_client,
+    get_parallel_backend,
 )
 from wildfires.data import (
     DATA_DIR,
@@ -64,7 +70,13 @@ from wildfires.data import (
 )
 from wildfires.joblib.cloudpickle_backend import register_backend as register_cl_backend
 from wildfires.logging_config import enable_logging
-from wildfires.utils import SimpleCache, Time, get_masked_array
+from wildfires.utils import (
+    SimpleCache,
+    Time,
+    get_masked_array,
+    shorten_columns,
+    shorten_features,
+)
 
 loguru_logger.enable("alepython")
 loguru_logger.remove()
@@ -87,9 +99,11 @@ mpl.rc("font", size=9.0)
 
 save_name = "fire_seasonality_paper"
 
+PROJECT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+
 figure_saver = FigureSaver(directories=os.path.join("~", "tmp", save_name), debug=True)
 memory = get_memory(save_name, verbose=100)
-CACHE_DIR = os.path.join(DATA_DIR, ".pickle", save_name)
+CACHE_DIR = Path(os.path.join(DATA_DIR, ".pickle", save_name))
 
 data_split_cache = SimpleCache("data_split", cache_dir=CACHE_DIR)
 cross_val_cache = SimpleCache("rf_cross_val", cache_dir=CACHE_DIR)
@@ -273,40 +287,225 @@ def get_shap_values(rf, X, data=None, interaction=False):
     return explainer.shap_values(X)
 
 
-def shorten_features(features):
-    if isinstance(features, str):
-        features = (features,)
+def save_ale_2d_and_get_importance(
+    model, train_set, features, n_jobs=8, include_first_order=False
+):
+    model.n_jobs = n_jobs
 
-    def month_repl(match_obj):
-        return f"{match_obj.group(1)} M"
+    fig, ax = plt.subplots(
+        figsize=(7.5, 4.5)
+    )  # Make sure plot is plotted onto a new figure.
+    with parallel_backend("threading", n_jobs=n_jobs):
+        fig, axes, (quantiles_list, ale, samples) = ale_plot(
+            model,
+            train_set,
+            features,
+            bins=20,
+            fig=fig,
+            ax=ax,
+            plot_quantiles=True,
+            quantile_axis=True,
+            plot_kwargs={
+                "colorbar_kwargs": dict(format="%.0e", pad=0.09, aspect=32, shrink=0.85)
+            },
+            return_data=True,
+            n_jobs=n_jobs,
+            include_first_order=include_first_order,
+        )
 
-    def delta_repl(match_obj):
-        return f"Δ{match_obj.group(1)} M"
+    # plt.subplots_adjust(top=0.89)
+    for ax_key in ("ale", "quantiles_x"):
+        axes[ax_key].xaxis.set_tick_params(rotation=45)
 
-    replacements = {
-        "-(\d+) - .*Month$": delta_repl,
-        "-(\d+) Month$": month_repl,
-        "VOD Ku-band": "VOD",
-        "Diurnal Temp Range": "DTR",
-        "Dry Day Period": "Dry Days",
-        re.escape("SWI(1)"): "SWI",
-    }
-    formatted = []
-    for feature in features:
-        for pattern, repl in replacements.items():
-            feature = re.sub(pattern, repl, feature)
-        formatted.append(feature)
-
-    if len(formatted) == 1:
-        return formatted[0]
-    return formatted
-
-
-def shorten_columns(df, inplace=False):
-    return df.rename(
-        columns=dict(
-            (orig, short)
-            for orig, short in zip(df.columns, shorten_features(df.columns))
-        ),
-        inplace=inplace,
+    figure_saver.save_figure(
+        fig,
+        "__".join(features),
+        sub_directory="2d_ale_first_order" if include_first_order else "2d_ale",
     )
+
+    #     min_samples = (
+    #         train_set.shape[0] / reduce(mul, map(lambda x: len(x) - 1, quantiles_list))
+    #     ) / 10
+    #     return np.ma.max(ale[samples_grid > min_samples]) - np.ma.min(
+    #         ale[samples_grid > min_samples]
+    #     )
+
+    return np.ptp(ale)
+
+
+def save_pdp_plot_2d(model, X_train, features, n_jobs):
+    model.n_jobs = n_jobs
+    with parallel_backend("threading", n_jobs=n_jobs):
+        pdp_interact_out = pdp.pdp_interact(
+            model=model,
+            dataset=X_train,
+            model_features=X_train.columns,
+            features=features,
+            num_grid_points=[20, 20],
+        )
+
+    fig, axes = pdp.pdp_interact_plot(
+        pdp_interact_out, features, x_quantile=True, figsize=(7, 8),
+    )
+    axes["pdp_inter_ax"].xaxis.set_tick_params(rotation=45)
+    figure_saver.save_figure(fig, "__".join(features), sub_directory="pdp_2d")
+
+
+def save_ale_plot_1d_with_ptp(
+    model,
+    X_train,
+    column,
+    n_jobs=8,
+    monte_carlo_rep=1000,
+    monte_carlo_ratio=100,
+    verbose=False,
+    monte_carlo=True,
+    center=False,
+):
+    model.n_jobs = n_jobs
+    with parallel_backend("threading", n_jobs=n_jobs):
+        fig, ax = plt.subplots(
+            figsize=(7.5, 4.5)
+        )  # Make sure plot is plotted onto a new figure.
+        out = ale_plot(
+            model,
+            X_train,
+            column,
+            bins=20,
+            monte_carlo=monte_carlo,
+            monte_carlo_rep=monte_carlo_rep,
+            monte_carlo_ratio=monte_carlo_ratio,
+            plot_quantiles=True,
+            quantile_axis=True,
+            rugplot_lim=0,
+            scilim=0.6,
+            return_data=True,
+            return_mc_data=True,
+            verbose=verbose,
+            center=center,
+        )
+    if monte_carlo:
+        fig, axes, data, mc_data = out
+    else:
+        fig, axes, data = out
+
+    for ax_key in ("ale", "quantiles_x"):
+        axes[ax_key].xaxis.set_tick_params(rotation=45)
+
+    sub_dir = "ale" if monte_carlo else "ale_non_mc"
+    figure_saver.save_figure(fig, column, sub_directory=sub_dir)
+
+    if monte_carlo:
+        mc_ales = np.array([])
+        for mc_q, mc_ale in mc_data:
+            mc_ales = np.append(mc_ales, mc_ale)
+        return np.ptp(data[1]), np.ptp(mc_ales)
+    else:
+        return np.ptp(data[1])
+
+
+def save_pdp_plot_1d(model, X_train, column, n_jobs):
+    data_file = os.path.join(CACHE_DIR, "pdp_data", column)
+
+    if not os.path.isfile(data_file):
+        model.n_jobs = n_jobs
+        with parallel_backend("threading", n_jobs=n_jobs):
+            pdp_isolate_out = pdp.pdp_isolate(
+                model=model,
+                dataset=X_train,
+                model_features=X_train.columns,
+                feature=column,
+                num_grid_points=20,
+            )
+        os.makedirs(os.path.dirname(data_file), exist_ok=True)
+        with open(data_file, "wb") as f:
+            pickle.dump((column, pdp_isolate_out), f, -1)
+    else:
+        with open(data_file, "rb") as f:
+            column, pdp_isolate_out = pickle.load(f)
+
+    # With ICEs.
+    fig_ice, axes_ice = pdp.pdp_plot(
+        pdp_isolate_out,
+        column,
+        plot_lines=True,
+        center=True,
+        frac_to_plot=1000,
+        x_quantile=True,
+        figsize=(7, 5),
+    )
+    axes_ice["pdp_ax"].xaxis.set_tick_params(rotation=45)
+    figure_saver.save_figure(fig_ice, column, sub_directory="pdp")
+
+    # Without ICEs.
+    fig_no_ice, ax = plt.subplots(figsize=(7.5, 4.5))
+    plt.plot(pdp_isolate_out.pdp - pdp_isolate_out.pdp[0], marker="o")
+    plt.xticks(
+        ticks=range(len(pdp_isolate_out.pdp)),
+        labels=_sci_format(pdp_isolate_out.feature_grids, scilim=0.6),
+        rotation=45,
+    )
+    plt.xlabel(f"{column}")
+    plt.title(f"PDP of feature '{column}'\nBins: {len(pdp_isolate_out.pdp)}")
+    plt.grid(alpha=0.4, linestyle="--")
+    figure_saver.save_figure(fig_no_ice, column, sub_directory="pdp_no_ice")
+    return (fig_ice, fig_no_ice), pdp_isolate_out, data_file
+
+
+def multi_ale_plot_1d(
+    model,
+    X_train,
+    columns,
+    fig_name,
+    xlabel=None,
+    ylabel=None,
+    title=None,
+    n_jobs=8,
+    verbose=False,
+):
+    fig, ax = plt.subplots(
+        figsize=(7.5, 4.5)
+    )  # Make sure plot is plotted onto a new figure.
+    model.n_jobs = n_jobs
+    with parallel_backend("threading", n_jobs=n_jobs):
+        quantile_list = []
+        ale_list = []
+        for feature in tqdm(
+            columns, desc="Calculating feature ALEs", disable=not verbose
+        ):
+            quantiles, ale = first_order_ale_quant(
+                model.predict, X_train, feature, bins=20
+            )
+            quantile_list.append(quantiles)
+            ale_list.append(ale)
+
+    # Construct quantiles from the individual quantiles, minimising the amount of interpolation.
+    combined_quantiles = np.vstack([quantiles[None] for quantiles in quantile_list])
+
+    final_quantiles = np.mean(combined_quantiles, axis=0)
+    # Account for extrema.
+    final_quantiles[0] = np.min(combined_quantiles)
+    final_quantiles[-1] = np.max(combined_quantiles)
+
+    mod_quantiles = np.arange(len(quantiles))
+    for feature, quantiles, ale in zip(columns, quantile_list, ale_list):
+        # Interpolate each of the quantiles relative to the accumulated final quantiles.
+        ax.plot(
+            np.interp(quantiles, final_quantiles, mod_quantiles),
+            ale,
+            marker="o",
+            ms=3,
+            label=feature,
+        )
+
+    ax.legend(loc="best")
+    ax.set_xticks(mod_quantiles)
+    ax.set_xticklabels(_sci_format(final_quantiles, scilim=0.6))
+    ax.xaxis.set_tick_params(rotation=45)
+    ax.grid(alpha=0.4, linestyle="--")
+
+    fig.suptitle(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    figure_saver.save_figure(fig, fig_name, sub_directory="multi_ale")
