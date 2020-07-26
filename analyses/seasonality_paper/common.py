16e1de8ff49647068531cd70d7180ca613310973
Alexander Kuhn-Regnier <ahk114@ic.ac.uk>
2020-07-26 22:16:11 +0100

    Improved plotting of predictions. Fixed feature importance combination plotting. Models without lagged dry day period and only lagged vegetation proxies.

--- analyses/seasonality_paper/common.py
+++ analyses/seasonality_paper/common.py
@@ -839,21 +839,28 @@ def transform_series_sum_norm(x):
 def plot_and_list_importances(importances, methods, print_n=15, N=15, verbose=True):
     fig, ax = plt.subplots()
 
+    transformed = {}
+
     combined = None
     for method in methods:
-        transformed = transform_series_sum_norm(importances[method])
-
-        ax.plot(transformed.values, linestyle="", marker="o", label=method)
-
+        transformed[method] = transform_series_sum_norm(importances[method])
         if combined is None:
-            combined = transformed
+            combined = transformed[method].copy()
         else:
-            combined += transformed
+            combined += transformed[method]
+    combined.sort_values(ascending=False, inplace=True)
 
+    transformed = pd.DataFrame(transformed).reindex(combined.index, axis=0)
+
+    for method, marker in zip(methods, ["o", "x", "s", "^"]):
+        ax.plot(
+            transformed[method], linestyle="", marker=marker, label=method, alpha=0.8
+        )
+    ax.set_xticklabels(
+        transformed.index, rotation=45 if len(transformed.index) <= 15 else 90
+    )
     ax.legend(loc="best")
     ax.grid(alpha=0.4)
-
-    combined.sort_values(ascending=False, inplace=True)
     if verbose:
         print(combined[:print_n].to_latex())
     return combined[:N]
